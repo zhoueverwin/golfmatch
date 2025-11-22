887b36e164055f833b87017899bf930fe6c3316b
zhoueverwin <zhou.wenbin.x2@gmail.com>
2025-11-22 20:20:41 +0900

    Fix Metro cache permission errors
    - Set custom TMPDIR to ~/.metro-tmp to avoid EACCES permission issues
    - Prevent metro-file-map from writing to system temp directories
    - Resolve persistent permission denied errors when running expo start

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,19 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+const os = require('os');
+
+// Set custom temp directory to avoid permission issues
+const customTempDir = path.join(os.homedir(), '.metro-tmp');
+process.env.TMPDIR = customTempDir;
 
 const config = getDefaultConfig(__dirname);
+
+// Use custom cache directory to avoid permission issues
+config.cacheStores = [
+  new (require('metro-cache').FileStore)({
+    root: path.join(os.homedir(), '.metro-cache'),
+  }),
+];
 
 config.transformer = {
   ...config.transformer,
