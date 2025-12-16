700b7daa196859afc36c2b1deda0edd4559350ad
Apple <apple@appurunoMacBook-Pro.local>
2025-12-16 17:16:42 +0900

    change version

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "Golfmatch",
     slug: "golfmatch",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
